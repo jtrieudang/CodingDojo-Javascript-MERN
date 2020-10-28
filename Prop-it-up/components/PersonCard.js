70ab72addc9aa987f5d228116639636bde17e3df
Jimmy T Trieudang <62171921+jtrieudang@users.noreply.github.com>
2020-10-28 00:18:40 -0700

    Add files via upload

---
+++ Prop-it-up/components/PersonCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const PersonCard = ({ 
+    firstName, 
+    lastName, 
+    age, 
+    hairColor }) => {
+
+    return(
+        <div>
+            <h2>{lastName}, {firstName}</h2>
+            <p>Age: {age}</p>
+            <p>Hair Color: {hairColor}</p>
+        </div>
+    );
+}
+
+export default PersonCard;
